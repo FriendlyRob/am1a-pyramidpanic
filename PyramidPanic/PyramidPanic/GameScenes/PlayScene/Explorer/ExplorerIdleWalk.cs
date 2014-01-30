aa609ae8fd0a08e7679f1c3d62c9b8437f967585
Rob van der Heijden <bunky_rob@hotmail.com>
2014-01-30 15:35:27 +0100

    idlewalk en walkdown van explorer gecomented

--- PyramidPanic/PyramidPanic/GameScenes/PlayScene/Explorer/ExplorerIdleWalk.cs
+++ PyramidPanic/PyramidPanic/GameScenes/PlayScene/Explorer/ExplorerIdleWalk.cs
@@ -10,24 +10,28 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
+// exporerwalk
 namespace PyramidPanic
 {
     // Dit is een toestands class (dus moet hij de interface implementeren)
-    // Deze class belooft dan plechtig dat hij de methods uit de interface heeft (toepast)
-    
+    // Deze class belooft dan plechtig dat hij de methods uit de interface heeft (toepast)  
     public class ExplorerIdleWalk : AnimatedSprite, IEntityState
     {
         //Fields
+        // Explorer object
         private Explorer explorer;
+        // velocity tijdens lopen
         private Vector2 velocity;
+        // imagenumber foor animate sprite
         private int imageNumber = 1;
        
         //properties
+        // effect (flippen)
         public SpriteEffects Effect
         {
             set { this.effect = value; }
         }
-
+        // rotatie
         public float Rotation
         {
             set { this.rotation = value; }
@@ -36,21 +40,28 @@ namespace PyramidPanic
         //Constructor
         public ExplorerIdleWalk(Explorer explorer) : base(explorer)
         {
+            // explorer object
             this.explorer = explorer;
+            // destination rectangle
             this.destinationRectangle = new Rectangle((int)this.explorer.Position.X,
                                                       (int)this.explorer.Position.Y,
                                                       32,
                                                       32);
+            // source rectangle
             this.sourceRectangle = new Rectangle(this.imageNumber * 32, 0, 32, 32);
+            // velocity
             this.velocity = new Vector2(0f, 0f);
         }
 
+        // initialize
         public void Initialize()
         {
+            // hier worden de x en de y geinitialiseerd
             this.destinationRectangle.X = (int)this.explorer.Position.X;
             this.destinationRectangle.Y = (int)this.explorer.Position.Y;
         }
 
+        // update
         public new void Update(GameTime gameTime)
         {
             //Bij het indrukken van de Right knop moet de toestand van de explorer veranderen in
@@ -61,18 +72,24 @@ namespace PyramidPanic
                 this.explorer.Idle.Effect = SpriteEffects.None;
                 this.explorer.Idle.Rotation = 0f;
             }
+            //Bij het indrukken van de Left knop moet de toestand van de explorer veranderen in
+            // ExplorerWalkleft
             else if (Input.EdgeDetectKeyUp(Keys.Left))
             {
                 this.explorer.State = this.explorer.Idle;
                 this.explorer.Idle.Effect = SpriteEffects.FlipHorizontally;
                 this.explorer.Idle.Rotation = 0f;
             }
+            //Bij het indrukken van de down knop moet de toestand van de explorer veranderen in
+            // ExplorerWalk down
             else if (Input.EdgeDetectKeyUp(Keys.Down))
             {
                 this.explorer.State = this.explorer.Idle;
                 this.explorer.Idle.Effect = SpriteEffects.None;
                 this.explorer.Idle.Rotation = (float)Math.PI / 2;
             }
+            //Bij het indrukken van de up knop moet de toestand van de explorer veranderen in
+            // ExplorerWalk up 
             else if (Input.EdgeDetectKeyUp(Keys.Up))
             {
                 this.explorer.State = this.explorer.Idle;
@@ -83,7 +100,7 @@ namespace PyramidPanic
             base.Update(gameTime);
         }
 
-
+        // draw
         public new void Draw(GameTime gameTime)
         {
             base.Draw(gameTime);
--- PyramidPanic/PyramidPanic/GameScenes/PlayScene/Explorer/ExplorerWalkDown.cs
+++ PyramidPanic/PyramidPanic/GameScenes/PlayScene/Explorer/ExplorerWalkDown.cs
@@ -18,44 +18,52 @@ namespace PyramidPanic
     public class ExplorerWalkDown : AnimatedSprite, IEntityState
     {
         //Fields
+        // explorer object
         private Explorer explorer;
+        // velocity
         private Vector2 velocity;
 
         //Contstructor
         public ExplorerWalkDown(Explorer explorer) : base(explorer)
         {
+            // exporer object
             this.explorer = explorer;
+            // dest renct
             this.destinationRectangle = new Rectangle((int)this.explorer.Position.X,
                                                       (int)this.explorer.Position.Y,
                                                       32,
                                                       32);
+            // velocity
             this.velocity = new Vector2(0f, this.explorer.Speed);
+            // rotation
             this.rotation = (float)Math.PI / 2;
         }
 
+        // initialize
         public void Initialize()
         {
+            // X + Y worden hier aangegeven
             this.destinationRectangle.X = (int)this.explorer.Position.X;
             this.destinationRectangle.Y = (int)this.explorer.Position.Y;
         }
 
+        // update
         public new void Update(GameTime gameTime)
         {
             // Deze code zorgt ervoor dat de explorer niet buiten de rechterrand
             // kan lopen.
             this.explorer.Position += this.velocity;
-
             if (this.explorer.Position.Y > 480 - 16)
             {
-                //Breng de explorer in de toestand Idle
+                //Breng de explorer in de toestand Idlewalk
                 this.explorer.Position -= this.velocity;
                 this.explorer.State = this.explorer.IdleWalk;
+                // effect
                 this.explorer.IdleWalk.Effect = SpriteEffects.None;
-                this.explorer.IdleWalk.Rotation = (float)Math.PI / 2;
-              
+                // rotatie
+                this.explorer.IdleWalk.Rotation = (float)Math.PI / 2;  
             }
             
-
             // Als de Right knop wordt losgelaten, dan moet de 
             // explorer weer in de toestand Idle komen
             if (Input.EdgeDetectKeyUp(Keys.Down))
@@ -65,11 +73,11 @@ namespace PyramidPanic
                 this.explorer.Idle.Rotation = (float)Math.PI / 2;
             }
 
-
+            // update gametime
             base.Update(gameTime);
         }
 
-
+        // draw
         public new void Draw(GameTime gameTime)
         {
             base.Draw(gameTime);
