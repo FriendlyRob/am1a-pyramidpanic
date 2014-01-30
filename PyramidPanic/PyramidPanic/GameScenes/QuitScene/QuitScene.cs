3fc8cfa226ce710e99a4434567ccbfc4be1f099e
Rob van der Heijden <bunky_rob@hotmail.com>
2014-01-30 16:03:53 +0100

    commitong quitscene here, its pretty empty but here is now a lot of comment there. btw im writing english because it just feels more natural and is easier/faster for me

--- PyramidPanic/PyramidPanic/GameScenes/QuitScene/QuitScene.cs
+++ PyramidPanic/PyramidPanic/GameScenes/QuitScene/QuitScene.cs
@@ -10,6 +10,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
+// quitscene
 namespace PyramidPanic
 {
     public class QuitScene : IState
