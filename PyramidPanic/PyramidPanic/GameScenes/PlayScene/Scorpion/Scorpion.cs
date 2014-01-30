67ad2f5b64faae86880058d31e380df0f21cc3fd
Rob van der Heijden <bunky_rob@hotmail.com>
2014-01-30 15:59:40 +0100

    scorpion walkleft and the original scorpion class comentated on

--- PyramidPanic/PyramidPanic/GameScenes/PlayScene/Scorpion/Scorpion.cs
+++ PyramidPanic/PyramidPanic/GameScenes/PlayScene/Scorpion/Scorpion.cs
@@ -10,46 +10,59 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
+// scorpion
 namespace PyramidPanic
 {
     public class Scorpion : IAnimatedSprite
     {
         //Fields
+        // game object
         private PyramidPanic game;
+        // ientity states
         private IEntityState state;
+        // texture for the sprite
         private Texture2D texture;
+        // speed of the scorpion
         private int speed = 2;
+        // the position of the scorpion
         private Vector2 position;
+        // the 2 things that scorpion can do, walkleft and walkright
         private WalkLeft walkLeft;
         private WalkRight walkRight;
 
-
         //properties
+        // walkleft
         public WalkLeft WalkLeft
         {
             get { return this.walkLeft; }
         }
+        // walkright
         public WalkRight WalkRight
         {
             get { return this.walkRight; }
         }
+        // postition (also has set so it can change)
         public Vector2 Position
         {
             get { return this.position; }
             set { this.position = value; }
         }
+        // states
         public IEntityState State
         {
             set { this.state = value; }
         }
+        // game
         public PyramidPanic Game
         {
             get { return this.game; }
         }
+        // speed of scorpion
         public int Speed
         {
             get { return this.speed; }
         }
+        // texture for the sprite that the scorpion uses
         public Texture2D Texture
         {
             get { return this.texture; }
@@ -58,11 +71,16 @@ namespace PyramidPanic
         //Constructor
         public Scorpion(PyramidPanic game, Vector2 position)
         {
+            // game object
             this.game = game;
+            // position of the scorpion
             this.position = position;
+            // texture (@"Scorpion\Scorpion" = path to the picture used for the scrpion)
             this.texture = game.Content.Load<Texture2D>(@"Scorpion\Scorpion");
+            // walkleft and right (the 2 thing he can do)
             this.walkLeft = new WalkLeft(this);
             this.walkRight = new WalkRight(this);
+            //states
             this.state = this.walkLeft;
         }
 
--- PyramidPanic/PyramidPanic/GameScenes/PlayScene/Scorpion/WalkLeft.cs
+++ PyramidPanic/PyramidPanic/GameScenes/PlayScene/Scorpion/WalkLeft.cs
@@ -10,50 +10,64 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
+// walkleft
 namespace PyramidPanic
 {
     // Dit is een toestands class (dus moet hij de interface implementeren)
-    // Deze class belooft dan plechtig dat hij de methods uit de interface heeft (toepast)
-    
+    // Deze class belooft dan plechtig dat hij de methods uit de interface heeft (toepast)  
     public class WalkLeft : AnimatedSprite, IEntityState
     {
         //Fields
+        // scorpion object
         private Scorpion scorpion;
+        // velocity of the scorpion when it moves
         private Vector2 velocity;
 
         //Contstructor
         public WalkLeft(Scorpion scorpion) : base(scorpion)
         {
+            // scorpion object
             this.scorpion = scorpion;
+            // effect (used for flipping)
             this.effect = SpriteEffects.FlipHorizontally;
+            // dest rect
             this.destinationRectangle = new Rectangle((int)this.scorpion.Position.X,
                                                       (int)this.scorpion.Position.Y,
                                                       32,
                                                       32);
+            // velocity of scorpion (used for the movement)
             this.velocity = new Vector2(this.scorpion.Speed, 0f);
         }
 
+        // init
         public void Initialize()
         {
+            // X+ y initialized here
             this.destinationRectangle.X = (int)this.scorpion.Position.X;
             this.destinationRectangle.Y = (int)this.scorpion.Position.Y;
         }
 
+        // update
         public new void Update(GameTime gameTime)
         {
+            // when it touches the end of screen change state so it 
+            // will alse change direction it will go
             if (this.scorpion.Position.X < (0+16))
             {
                 //Breng de beetle in de toestand walkdown
                 this.scorpion.State = this.scorpion.WalkRight;
                 this.scorpion.WalkRight.Initialize();
             }
+            // calculating the position of the scorpion using the velocity
             this.scorpion.Position -= this.velocity;
+            // X+Y
             this.destinationRectangle.X = (int)this.scorpion.Position.X;
             this.destinationRectangle.Y = (int)this.scorpion.Position.Y;
+            // update of hte gametiem
             base.Update(gameTime);
         }
 
-
+        // draw
         public new void Draw(GameTime gameTime)
         {
             base.Draw(gameTime);
